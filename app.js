670fcbdf4a1e8c7ef4dfe3cf2e83bc6de340288a
Michele Abategiovanni <112349713+MicheleAbategiovanni@users.noreply.github.com>
2024-05-03 20:29:27 +0200

    adds view html and routes

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/products',(req,res,next) => {
-    console.log('Second middleware');
-    res.send('<h1>H3LL0 3xpressJS</h1>')
+app.use("/admin", adminRoute);
+app.use(shopRoute);
+
+// Error 404 middleware not found
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "../", "views", "error.html"));
 });
 
-app.use('/',(req,res,next) => {
-    console.log('First middleware')
-    res.send('<h1>H3LL0 3xpressJS</h1>')
-});
-
-app.listen(3000, () => console.log("W3LC0M3 M1K3 ! \nServer is running on port 3000"));
+app.listen(3000, () =>
+  console.log("W3LC0M3 M1K3 ! \nServer is running on port 3000")
+);
